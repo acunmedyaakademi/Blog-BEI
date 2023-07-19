edbde82cd60556339f5072ebc314488bbc2e7beb
ibrahmjk1903 <110205635+ibrahmjk1903@users.noreply.github.com>
2023-07-19 18:06:32 +0300

    flask post yazıldı

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, jsonify, request, render_template
+from datetime import datetime
+import sqlite3
+
+created_on = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
 app = Flask(__name__, '/assets', 'assets')
 
@@ -12,7 +16,29 @@ def about():
 
 @app.route('/post')
 def post():
-    return render_template('post.html')
+    if request.method == 'POST':
+        conn = sqlite3.connect('blog.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM post')
+        rows = cursor.fetchall()
+        blog_data = []
+        for row in rows:
+            blog_data.append({
+                'id': row[0],
+                'post_id': row[1],
+                'title': row[2],
+                'description': row[3],
+                'created_on' : row[4]
+            })
+        conn.close()
+    return render_template('post.html',blog_data=blog_data)
+
+
+
+    
+
+
+
 
 @app.route('/contact')
 def contact():
